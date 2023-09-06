ca6844fd0abe52568ab69038481d73f83901b930
superKazi <kazi.awal@journalism.cuny.edu>
2023-09-06 13:11:53 -0400

    dynamic routes and rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://astro-frontend-masters.netlify.app',
 	integrations: [react(), solidJs()],
 });
