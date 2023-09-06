3e853b4a1f5f85b6d508eec98db683480d241c25
superKazi <kazi.awal@journalism.cuny.edu>
2023-09-06 13:33:57 -0400

    hybrid rendering and forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import solidJs from '@astrojs/solid-js';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://astro-frontend-masters.netlify.app',
+	output: 'hybrid',
 	integrations: [react(), solidJs()],
 });
